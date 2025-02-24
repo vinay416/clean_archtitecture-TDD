22deb3978c464c249b97a2b0958149359c85ca8d
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-02-24 13:40:19 +0530

    GREEN - test case 'GetRandomNumberTrivia'

---
+++ lib/features/number_trivia/domain/usecases/get_random_number_trivia.dart
@@ -0,0 +1,13 @@
+import 'package:clean_architecture/core/errors/failure.dart';
+import 'package:clean_architecture/features/number_trivia/domain/entities/number_triva_entity.dart';
+import 'package:clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetRandomNumberTrivia {
+  final NumberTriviaRepository repo;
+  GetRandomNumberTrivia(this.repo);
+
+  Future<Either<Failure, NumberTrivaEntity>> execute () async {
+    return await repo.getRandomNumberTrivia();
+  }
+}
