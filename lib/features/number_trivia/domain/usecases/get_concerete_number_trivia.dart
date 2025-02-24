70d06d64035e1fefa93d9c6f79e19d1098ed4cf1
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-02-24 13:39:12 +0530

    GREEN - test passed 'GetConcereteNumberTrivia'

---
+++ lib/features/number_trivia/domain/usecases/get_concerete_number_trivia.dart
@@ -0,0 +1,13 @@
+import 'package:clean_architecture/core/errors/failure.dart';
+import 'package:clean_architecture/features/number_trivia/domain/entities/number_triva_entity.dart';
+import 'package:clean_architecture/features/number_trivia/domain/repositories/number_trivia_repository.dart';
+import 'package:dartz/dartz.dart';
+
+class GetConcreteNumberTrivia {
+  final NumberTriviaRepository repo;
+  GetConcreteNumberTrivia(this.repo);
+
+  Future<Either<Failure, NumberTrivaEntity>> execute (int number) async {
+    return await repo.getConcreteNumberTrivia(number);
+  }
+}
