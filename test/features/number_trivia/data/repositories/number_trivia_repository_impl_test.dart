4564eabfebfb02c7e24458af531539109083acf2
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-02-26 11:56:07 +0530

    RED - "Device is online and API call success"

--- test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
+++ test/features/number_trivia/data/repositories/number_trivia_repository_impl_test.dart
@@ -1,4 +1,8 @@
+import 'package:clean_architecture/core/errors/exceptions.dart';
+import 'package:clean_architecture/core/errors/failure.dart';
+import 'package:clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:clean_architecture/features/number_trivia/data/repositories/number_trivia_repository_impl.dart';
+import 'package:dartz/dartz.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/mockito.dart';
 
@@ -30,15 +34,35 @@ void main() {
   );
 
   group(
-    "NumberTriviaRepositoryImpl Test",
+    "Get Remote Data Source Test",
     () {
-      const tNumber = 1;
-      test(
-        "Network Connected",
+      setUp(
         () {
           when(mockNetworkInfo.isConnected()).thenAnswer((_) async => true);
-          triviaRepoImpl.getConcreteNumberTrivia(tNumber);
-          verify(mockNetworkInfo.isConnected());
+        },
+      );
+      const tNumber = 1;
+      const remoteData =
+          NumberTriviaModel(text: "Test trivia", number: tNumber);
+      test(
+        "Device is online and API call success",
+        () async {
+          when(mockRemoteDataSource.getConcreteNumberTrivia(any))
+              .thenAnswer((_) => Future.value(remoteData));
+          final result = await triviaRepoImpl.getConcreteNumberTrivia(tNumber);
+          verify(mockRemoteDataSource.getConcreteNumberTrivia(tNumber));
+          expect(result, const Right(remoteData));
+        },
+      );
+
+      test(
+        "Device is online and API call failed",
+        () async {
+          when(mockRemoteDataSource.getConcreteNumberTrivia(any))
+              .thenThrow((_) => (ServerException()));
+          final result = await triviaRepoImpl.getConcreteNumberTrivia(tNumber);
+          verify(mockRemoteDataSource.getConcreteNumberTrivia(tNumber));
+          expect(result, Left(ServerFailure()));
         },
       );
     },
