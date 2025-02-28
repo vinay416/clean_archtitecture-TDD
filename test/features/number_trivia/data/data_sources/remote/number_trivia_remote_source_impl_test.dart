5c1fa0a766ceb85a5b45553ffdb0896be4f4e17f
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-02-28 17:16:28 +0530

    RED - Get concrete trivia when API fails, throw Exception

--- test/features/number_trivia/data/data_sources/remote/number_trivia_remote_source_impl_test.dart
+++ test/features/number_trivia/data/data_sources/remote/number_trivia_remote_source_impl_test.dart
@@ -1,5 +1,6 @@
 import 'dart:convert';
 
+import 'package:clean_architecture/core/errors/exceptions.dart';
 import 'package:clean_architecture/features/number_trivia/data/data_sources/number_trivia_remote_source.dart';
 import 'package:clean_architecture/features/number_trivia/data/models/number_trivia_model.dart';
 import 'package:dio/dio.dart';
@@ -25,6 +26,7 @@ void main() {
           const url = "$CONCRETE_TRIVIA_API$tNumber";
           when(dioMock.get(any)).thenAnswer((_) => Future.value(Response(
                 data: json.decode(Fixtures().call("trivia.json")),
+                statusCode: 200,
                 requestOptions: RequestOptions(
                   path: url,
                   contentType: "application/json",
@@ -34,6 +36,24 @@ void main() {
           final result = await remoteSource.getConcreteNumberTrivia(tNumber);
           verify(dioMock.get(url));
           expect(result, triviaModel);
+        },
+      );
+
+      test(
+        "Get concrete trivia when API fails, throw Exception",
+        () async {
+          const url = "$CONCRETE_TRIVIA_API$tNumber";
+          when(dioMock.get(any)).thenAnswer((_) => Future.value(Response(
+                statusCode: 500,
+                requestOptions: RequestOptions(
+                  path: url,
+                  contentType: "application/json",
+                ),
+              )));
+
+          final result = remoteSource.getConcreteNumberTrivia(tNumber);
+          verify(dioMock.get(url));
+          expect(() async => await result, throwsA(const TypeMatcher<ServerException>()));
         },
       );
     },
