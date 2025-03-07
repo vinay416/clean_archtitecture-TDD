d6416a2c67e48bf814a1c0eecab5e44f80ceda0f
Vinay_Sharma <vinaysharma31.12.1999@gmail.com>
2025-03-07 13:41:45 +0530

    Number remote source fixe

--- lib/features/number_trivia/data/data_sources/number_trivia_remote_source.dart
+++ lib/features/number_trivia/data/data_sources/number_trivia_remote_source.dart
@@ -8,9 +8,9 @@ abstract class NumberTriviaRemoteSource {
 }
 
 // ignore: constant_identifier_names
-const CONCRETE_TRIVIA_API = 'numberapi.com/';
+const CONCRETE_TRIVIA_API = 'http://numberapi.com/';
 // ignore: constant_identifier_names
-const RANDOM_TRIVIA_API = 'numberapi.com/random';
+const RANDOM_TRIVIA_API = 'http://numberapi.com/random';
 
 class NumberTriviaRemoteSourceImpl implements NumberTriviaRemoteSource {
   const NumberTriviaRemoteSourceImpl(this.dioClient);
@@ -19,19 +19,27 @@ class NumberTriviaRemoteSourceImpl implements NumberTriviaRemoteSource {
   @override
   Future<NumberTriviaModel> getConcreteNumberTrivia(int number) async {
     final url = "$CONCRETE_TRIVIA_API$number";
-    final Response response = await dioClient.get(url);
-    if (response.statusCode == 200) {
-      return NumberTriviaModel.fromJson(response.data);
+    try {
+      final Response response = await dioClient.get(url);
+      if (response.statusCode == 200) {
+        return NumberTriviaModel.fromJson(response.data);
+      }
+      throw response;
+    } catch (e) {
+      throw ServerException();
     }
-    throw ServerException();
   }
 
   @override
   Future<NumberTriviaModel> getRandomNumberTrivia() async {
-    final Response response = await dioClient.get(RANDOM_TRIVIA_API);
-    if (response.statusCode == 200) {
-      return NumberTriviaModel.fromJson(response.data);
+    try {
+      final Response response = await dioClient.get(RANDOM_TRIVIA_API);
+      if (response.statusCode == 200) {
+        return NumberTriviaModel.fromJson(response.data);
+      }
+      throw response;
+    } catch (e) {
+      throw ServerException();
     }
-    throw ServerException();
   }
 }
